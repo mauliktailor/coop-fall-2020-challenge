ac576aebece62a2618a7ba3646c6695c89d87c02
mauliktailor <maulikrtailor@gmail.com>
2020-07-10 13:33:48 -0400

    Initial Commit`

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,53 @@
 class EventSourcer():
     # Do not change the signature of any functions
-
+    
     def __init__(self):
         self.value = 0
+        # to maintain the history of undo
+        self.history_undo=[]
+        # to maintain the histoy of redo
+        self.history_redo=[]
 
     def add(self, num: int):
-        pass
+        self.value+=num
+        self.history_undo.append((num,'+'))
 
     def subtract(self, num: int):
-        pass
+        self.value-=num
+        self.history_undo.append((num,'-'))
 
     def undo(self):
-        pass
+        if len(self.history_undo) == 0:
+            return
+        num, sign = self.history_undo[-1]
+        del self.history_undo[-1]
+        if sign == '+':
+            self.value -=num
+        else:
+            self.value +=num
+        self.history_redo.append((num, sign))
+
 
     def redo(self):
-        pass
+        if len(self.history_redo) == 0:
+            return
+        num, sign = self.history_redo[-1]
+        del self.history_redo[-1]
+        if sign == '+':
+            self.value +=num
+        else:
+            self.value -=num
+        
+        self.history_undo.append((num, sign))
 
     def bulk_undo(self, steps: int):
-        pass
+        if steps > len(self.history_undo):
+            steps = len(self.history_undo)
+        for i in range(steps):
+            self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        if steps > len(self.history_redo):
+            steps = len(self.history_redo)
+        for i in range(steps):
+            self.redo()
